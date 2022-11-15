41035c65d6b4549a37c079a47155799b1b96b588
MARTINS Romain <o22104786@info23-26.iut45.univ-orleans.fr>
2022-11-15 12:28:59 +0100

    Ajout des datas de amazon avec yaml

--- tuto/views.py
+++ tuto/views.py
@@ -1,6 +1,12 @@
 from .app import app
 from flask import render_template
+import yaml , os.path
+from yaml import Loader
 @app.route("/")
 
+
+
+
 def home():
-    return render_template("home.html",title ="Hello World!",names =["Pierre", "Paul", "Corinne"])
+    return render_template("home.html",title ="Hello World!",names =["Pierre", "Paul", "Corinne"]
+                           ,data = yaml.load(open("tuto/static/data.yml"),Loader=Loader))
