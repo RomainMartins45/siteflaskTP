ae979c8f1a2fa65fb3d7b8b8da8580863df56348
MARTINS Romain <o22104786@info23-25.iut45.univ-orleans.fr>
2022-11-22 12:09:49 +0100

    Mise en place BD

--- tuto/app.py
+++ tuto/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os.path
 
 app = Flask(__name__)
 Bootstrap(app)
+
+def mkpath(p):
+    return os.path.normpath(
+    os.path.join(
+    os.path.dirname(__file__),
+    p))
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+ mkpath("../myapp.db"))
+db = SQLAlchemy(app)
--- tuto/models.py
+++ tuto/models.py
@@ -1,14 +1,17 @@
-import yaml, os.path
-Books = yaml.safe_load(
-    open(
-        os.path.join(
-            os.path.dirname(__file__),
-            "static/data.yml")))
-# Pour avoir un id
-i = 0
-for book in Books:
-    book['id'] = i
-    i += 1
+from .app import db
+
+class Author(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String(100))
+
+class Book(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    price = db.Column(db.Float)
+    title = db.Column(db.String(100))
+    url = db.Column(db.String(200))
+    img = db.Column(db.String(200))
+    author_id = db.Column(db.Integer , db.ForeignKey("author.id"))
+    author = db.relationship("Author",backref=db.backref("books", lazy="dynamic"))
 
 def get_sample():
-    return Books[0:10]
+return Book.query.limit(10).all()
